b7e1a098af574a19e85c72c5106f9cf2ceb9fdd8
xnekj <xnekj005@studenti.czu.cz>
2025-04-14 00:42:58 +0200

    debug changed

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g-p3$t3njv#5z^o&dtj+f1lv(0!%2d%53m7-h2^30enwoe1_1i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 
